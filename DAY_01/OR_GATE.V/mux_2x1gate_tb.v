f4faec069ddda065348aa8baeb553c44f0380e6e
mahendiran1202 <mahemaahi07@gmail.com>
2025-12-08 15:03:48 +0530

    Update mux_2x1gate_tb.v

--- DAY_01/OR_GATE.V/mux_2x1gate_tb.v
+++ DAY_01/OR_GATE.V/mux_2x1gate_tb.v
@@ -6,9 +6,9 @@ initial begin
 $monitor ("time=%t i0=%b i1=%b y=%b",$time,i0,i1,s,y);
 $dumpfile("mux.vcd");
 $dumpvars(0);
+i0=0;i1=0;s=0;#10;
+i0=0;i1=1;s=1;#10;
 i0=1;i1=0;s=0;#10;
-i0=1;i1=0;s=1;#10;
-i0=0;i1=1;s=0;#10;
 i0=0;i1=1;s=1;#10;
 $finish;
 end 
